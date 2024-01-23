6e91ea0772b155799ace3b74ec0bbeefdbb5a136
Nash <116047357+NWMorrison@users.noreply.github.com>
2024-01-22 21:47:36 -0500

    Fix_Final

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/tip_Calculator")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Tip_Calculator_Final")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
